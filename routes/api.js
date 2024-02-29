b840f83d882608748de006e2b92f54d451f3f11a
eyadarouaz <eyaadarouez@gmail.com>
2024-02-29 20:45:12 +0100

    refactor: add necessary routes

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,26 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route('/api/convert')
+  .get((req, res) => {
+    let {input} = req.query;
+    
+    if(input) {
+      let initNum = convertHandler.getNum(input);
+      let initUnit = convertHandler.getUnit(input);
+
+      if(initNum === 'invalid number' && initUnit === 'invalid unit')
+        return res.json('invalid number and unit');
+      if(initNum === 'invalid number' && initUnit !== 'invalid unit')
+        return res.json('invalid number');
+      if(initNum !== 'invalid number' && initUnit === 'invalid unit')
+        return res.json('invalid unit');
+      
+      let returnNum = convertHandler.convert(initNum, initUnit);
+      let returnUnit = convertHandler.getReturnUnit(initUnit);
+
+      return res.json(convertHandler.getString(initNum, initUnit, returnNum, returnUnit));
+    }
+  })
+
 };
