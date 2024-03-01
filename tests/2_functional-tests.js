2cc1eb4859974aa96428533da2db29cb077a6a21
eyadarouaz <eyaadarouez@gmail.com>
2024-03-01 11:07:44 +0100

    test: add functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,67 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+    test('Test GET /api/convert?input=10L', function (done) {
+        chai
+          .request(server)
+          .keepOpen()
+          .get('/api/convert')
+          .query({input: '10L'})
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.initNum, 10);
+            assert.equal(res.body.initUnit, "L");
+            assert.equal(res.body.returnNum, 2.64172);
+            assert.equal(res.body.returnUnit, "gal");
+            done();
+        });
+    });
+    test('Test GET /api/convert?input=32g', function (done) {
+        chai
+          .request(server)
+          .keepOpen()
+          .get('/api/convert')
+          .query({input: '32g'})
+          .end(function (err, res) {
+            assert.equal(res.body, "invalid unit");
+            done();
+        });
+    });
+    test('Test GET /api/convert?input=3/7.2/4kg', function (done) {
+        chai
+          .request(server)
+          .keepOpen()
+          .get('/api/convert')
+          .query({input: '3/7.2/4kg'})
+          .end(function (err, res) {
+            assert.equal(res.body, "invalid number");
+            done();
+        });
+    });
+    test('Test GET /api/convert?input=3/7.2/4kilomegagram', function (done) {
+        chai
+          .request(server)
+          .keepOpen()
+          .get('/api/convert')
+          .query({input: '3/7.2/4kilomegagram'})
+          .end(function (err, res) {
+            assert.equal(res.body, "invalid number and unit");
+            done();
+        });
+    });
+    test('Test GET /api/convert?input=kg', function (done) {
+        chai
+          .request(server)
+          .keepOpen()
+          .get('/api/convert')
+          .query({input: 'kg'})
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.initNum, 1);
+            assert.equal(res.body.initUnit, "kg");
+            assert.equal(res.body.returnNum, 2.20462);
+            assert.equal(res.body.returnUnit, "lbs");
+            done();
+        });
+    });
 });
